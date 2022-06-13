12ddff081932b7b1c19db0c6fe6c1e23734b6d98
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-12 20:58:04 -0700

    Solution exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+  for (let i = 0; i < elements.length; i++) {
+    array = array.filter(entry => entry !== elements[i])
+  }
+  return array
 };
 
 // Do not edit below this line
