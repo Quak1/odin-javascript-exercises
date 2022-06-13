c8f20db155dac3acd9f2c669c32593bfab4e8540
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-12 21:20:35 -0700

    Solution exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  c = (f - 32) * 5 / 9;
+  return Number(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f = c * 9 / 5 + 32;
+  return Number(f.toFixed(1));
 };
 
 // Do not edit below this line
