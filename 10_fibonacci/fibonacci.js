b8fa2dbf52ecf6d0343a5f94af8584674bfa36d3
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-14 14:19:42 -0700

    Solution exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
 
+  let a = 0;
+  let b = 1;
+  let c, i;
+
+  for (i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
