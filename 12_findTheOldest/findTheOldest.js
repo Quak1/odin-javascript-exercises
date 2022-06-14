3604cd1c5dedf5e39f87ab81d1787249dd6e8354
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-14 14:41:15 -0700

    Solution exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+  return persons.reduce((oldest, current) => {
+    oldestAge = getAge(oldest);
+    currentAge = getAge(current);
+    return oldestAge >= currentAge ? oldest : current;
+  });
 };
+
+function getAge(person) {
+  if (!("yearOfDeath" in person)) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+  }
+
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
