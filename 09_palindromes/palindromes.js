50d2b15fb12fa0f44d8310606b6d2085e4e2272c
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-14 14:12:38 -0700

    Solution exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  text = text.toLowerCase().replace(/[.,/#!$%^&*;:{}=-_`~() ]/g, "");
 
+  const size = text.length;
+  const iter = (size / 2).toFixed();
+  for (let i = 0; i < iter; i++) {
+    if (text[i] !== text[size - 1 - i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
